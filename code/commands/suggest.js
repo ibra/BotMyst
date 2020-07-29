3bb95cf609a641092f17d5d05de2a2661c663b96
Ibrahim Hisham <61324615+minidevz@users.noreply.github.com>
2020-07-29 16:47:13 +0500

    Updated suggest.js

--- code/commands/suggest.js
+++ code/commands/suggest.js
@@ -1,5 +1,5 @@
 var Trello = require("trello");
-var trello = new Trello("19c3f00e729c2ea5e1be8d7931422dd1", "64dcea1f14c2a8a838c2aa941e50de727bf359f4c4b38d6892bfb2f46380cff3");
+var trello = new Trello("Application Token, List Token");
 const Discord = require('discord.js')
 
 module.exports = {
