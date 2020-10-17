aee94c1e7d844f2838b0cfdf40789217abbc46b6
minidevz <muhammadibrahimhisham@gmail.com>
2020-10-18 02:12:21 +0500

    testing pages

--- code/commands/word.js
+++ code/commands/word.js
@@ -7,7 +7,7 @@ module.exports = {
 	description: 'Search something and get the synonyms of that word',
     
     async run (client, message, args){
-    let pages = [];
+    let pages = ['Wow a page', 'Wow another one', 'Damn bro stop with all these pages', 'goddamn theres another one', 'bruh another one?', 'bruh no cap seriously stop', 'K thanks for stopping'];
     let page = 1;
     const wordEmbed = new Discord.MessageEmbed()
 	.setColor(3066993)
@@ -16,29 +16,38 @@ module.exports = {
     .setFooter(`Page ${page} of ${pages.length}`)
     .setTimestamp()
     message.channel.send(wordEmbed).then(msg => {
-        message.react('⬅️').then(r => {
-        message.react('➡️')    
+        msg.react('⬅️').then(r => {
+        msg.react('➡️')    
         
-        const backwardsReaction = reaction.emoji.name === '⬅️' && user.id === message.author.id;
-        const forwardReaction = reaction.emoji.name === '➡️' && user.id === message.author.id;
+        const backwardsReaction = (reaction,user) => reaction.emoji.name === '⬅️' && user.id === message.author.id;
+        const forwardReaction = (reaction,user) => reaction.emoji.name === '➡️' && user.id === message.author.id;
 
-        const backwards = message.createReactionCollector(backwardsReaction, {time: 50000})
-        const forwards = message.createReactionCollector(forwardReaction, {time: 50000})
+        const backwards = msg.createReactionCollector(backwardsReaction, {time: 50000})
+        const forwards = msg.createReactionCollector(forwardReaction, {time: 50000})
 
         backwards.on('collect', r =>{
         if(page === 1) return;
         page--;
         wordEmbed.setDescription(pages[page-1])
         wordEmbed.setFooter(`Page ${page} of ${pages.length}`);
-        message.edit(wordEmbed);
-
+        msg.edit(wordEmbed);
         })
+      
+        forwards.on('collect', r =>{
+            if(page === pages.length) return;
+            page++;
+            wordEmbed.setDescription(pages[page-1])
+            wordEmbed.setFooter(`Page ${page} of ${pages.length}`);
+            msg.edit(wordEmbed);
+    
+            })
+       
         })
         
         
     })
-  //  const wordLookup = args.join(" "); 
-//res = thesaurus.find(wordLookup);
+     //  const wordLookup = args.join(" "); 
+    //res = thesaurus.find(wordLookup);
       }
     
 
