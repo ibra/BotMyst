04d98d7fc5533d85631c533ffb7d94d169721364
minidevz <muhammadibrahimhisham@gmail.com>
2020-10-18 11:10:48 +0500

    Update help.js

--- code/commands/help.js
+++ code/commands/help.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const config = require('../config.json');
 
 module.exports = {
     name: "help",
@@ -7,21 +8,15 @@ module.exports = {
    async run (client, message, args) {
       const data = [];
       const { commands } = message.client;
-    
-       var embed = new Discord.MessageEmbed()
-       var desc = data.push(commands.map(command => command.name).join(', '));
-       embed.setTitle("Here's a list of all my commands:");
-       embed.setColor("RANDOM");
-       embed.setDescription(desc);
-       embed.setFooter(`You can send >help [command name]\` to get info on a specific command!`);
-       return message.author.send(data, { split: true })
+      var desc = data.push(commands.map(command => command.name).join(', '));  
+      return message.author.send(data, { split: true })
         .then(() => {
         if (message.channel.type === 'dm') return;
         message.reply('I\'ve sent you a DM with all my commands!');
       })
       .catch(error => {
         console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-        message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+        message.reply('Uh oh. Something went wrong when trying to DM you');
       });
   }
 }
