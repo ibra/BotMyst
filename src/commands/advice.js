599b365ca829e5536e9e24bf99055f706e99031b
Harsh Singh <harshhh.singh.personal@gmail.com>
2020-10-18 20:42:19 -0400

    Added advice command.

---
+++ src/commands/advice.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "advice",
+    description: "Gets much needed life advice",
+    execute: async function(message) {
+        request
+        .get('http://api.adviceslip.com/advice')
+        .end((err, res) => {
+            if (!err && res.status === 200) {
+                try {
+                    JSON.parse(res.text)
+                } catch (e) {
+                    return message.reply(', an api error occurred.');
+                }
+                const advice = JSON.parse(res.text)
+                message.channel.send(advice.slip.advice)
+            } else {
+            console.error(`REST call failed: ${err}, status code: ${res.status}`)
+            }
+        });
+    }
+}
