ed94fd77a27fae527599c5508d21c9e97c3ff736
Harsh Singh <harshhh.singh.personal@gmail.com>
2020-10-18 22:11:32 -0400

    Added f command.

---
+++ src/commands/f.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "f",
+    description: "Pays respect.",
+    aliases: [],
+
+    execute: async function (client, message, args) {
+        message.channel.send('f');
+    }
+}
