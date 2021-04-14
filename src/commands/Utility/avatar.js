823dcfd82dd296be33bf30c29bbb3f97c0156e0a
codemyst <code@myst.rs>
2021-04-14 13:31:59 +0200

    refactored avatar.js

--- src/commands/Utility/avatar.js
+++ src/commands/Utility/avatar.js
@@ -5,25 +5,22 @@ import { Colors } from "../../colors.js";
 export const name = "avatar";
 export const description = "Returns mentioned user's avatar and returns your avatar if there are no arguments";
 export const aliases = ['av'];
-export const usage = `${Prefix}avatar @User#001 [Returns Their Avatar]`;
+export const usage = `${Prefix}avatar @User#001 - Returns Their Avatar`;
 export const category = "Utility";
 
 export async function execute(client, message, args) {
-    // Create new message Embed 
     const avatarEmbed = new MessageEmbed();
 
-    // If there is no mention, then display Author's avatar.
+    let user = undefined;
     if (!message.mentions.users.first()) {
-        avatarEmbed.setTitle(`${user.username}'s avatar!`);
-        avatarEmbed.setColor(Colors.ORANGE);
-        avatarEmbed.setImage(message.author.displayAvatarURL());
-    } // Otherwise, we display the mentioned users avatar.
-    else {
-        const user = message.mentions.users.first();
-        avatarEmbed.setTitle(`${user.username}'s avatar!`);
-        avatarEmbed.setColor(Colors.ORANGE);
-        avatarEmbed.setImage(user.displayAvatarURL());
+        user = message.author;
+    } else {
+        user = message.mentions.users.first();
     }
+
+    avatarEmbed.setTitle(`${user.username}'s avatar!`);
+    avatarEmbed.setColor(Colors.ORANGE);
+    avatarEmbed.setImage(user.displayAvatarURL());
 
     message.channel.send(avatarEmbed);
 }
