b7be487e0c405c8565f0ca832aea09f48d93bdbe
ibra <muhammadibrahimhisham@gmail.com>
2021-05-18 13:29:46 +0500

    updated command aliases
    Basically for both wiki.js and dict.js, the aliases and the command name had been switched around, so the alias name was the command name, and vice versa.

--- src/commands/Core/dict.js
+++ src/commands/Core/dict.js
@@ -3,49 +3,49 @@ import fetch from "node-fetch";
 import { Prefix } from "../../config.js";
 import { Colors } from "../../colors.js";
 
-export const name = "dict";
+export const name = "dictionary";
 export const description = "A command that shows the definition of a word";
-export const aliases = ["dictionary"];
+export const aliases = ["dict"];
 export const usage = `${Prefix}dict idiot [returns definition of word]`;
 export const category = "Core";
 
 export async function execute(client, message, args) {
-    //Remove Prefix.
-    const wordLookup = args.join(" ");
-    //Create new embed.
-    const dictionaryEmbed = new MessageEmbed();
+  //Remove Prefix.
+  const wordLookup = args.join(" ");
+  //Create new embed.
+  const dictionaryEmbed = new MessageEmbed();
 
-    if (wordLookup == "") {
-        dictionaryEmbed.setAuthor("> Error 400");
-        dictionaryEmbed.setDescription("You havent provided any arguments!");
-        dictionaryEmbed.setColor(Colors.RED);
-        message.channel.send(dictionaryEmbed);
-        return;
-    }
+  if (wordLookup == "") {
+    dictionaryEmbed.setAuthor("> Error 400");
+    dictionaryEmbed.setDescription("You havent provided any arguments!");
+    dictionaryEmbed.setColor(Colors.RED);
+    message.channel.send(dictionaryEmbed);
+    return;
+  }
 
-    const requestURL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-    const results = await fetch(`${requestURL}/${wordLookup}`);
-    const json = await results.json();
+  const requestURL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+  const results = await fetch(`${requestURL}/${wordLookup}`);
+  const json = await results.json();
 
-    if (json[0] != null) {
-        var phonetics = json[0].phonetics[0].text;
-        var meaning = json[0].meanings[0].definitions[0].definition;
-        var example = json[0].meanings[0].definitions[0].example;
+  if (json[0] != null) {
+    var phonetics = json[0].phonetics[0].text;
+    var meaning = json[0].meanings[0].definitions[0].definition;
+    var example = json[0].meanings[0].definitions[0].example;
 
-        dictionaryEmbed.setAuthor(`${json[0].word} | ${phonetics}`);
-        if (example != null) {
-            dictionaryEmbed.setFooter("e.g: " + example);
-        } else {
-            dictionaryEmbed.setFooter("");
-        }
-
-        dictionaryEmbed.setDescription(meaning);
-        dictionaryEmbed.setColor(Colors.ORANGE);
-        message.channel.send(dictionaryEmbed);
+    dictionaryEmbed.setAuthor(`${json[0].word} | ${phonetics}`);
+    if (example != null) {
+      dictionaryEmbed.setFooter("e.g: " + example);
     } else {
-        dictionaryEmbed.setAuthor("> Error 404");
-        dictionaryEmbed.setDescription("Sorry! I was unable to find that word.");
-        dictionaryEmbed.setColor(Colors.RED);
-        message.channel.send(dictionaryEmbed);
+      dictionaryEmbed.setFooter("");
     }
+
+    dictionaryEmbed.setDescription(meaning);
+    dictionaryEmbed.setColor(Colors.ORANGE);
+    message.channel.send(dictionaryEmbed);
+  } else {
+    dictionaryEmbed.setAuthor("> Error 404");
+    dictionaryEmbed.setDescription("Sorry! I was unable to find that word.");
+    dictionaryEmbed.setColor(Colors.RED);
+    message.channel.send(dictionaryEmbed);
+  }
 }
--- src/commands/Core/wiki.js
+++ src/commands/Core/wiki.js
@@ -5,30 +5,32 @@ import { Colors } from "../../colors.js";
 
 const Logger = new UtilLogger();
 
-export const name = "wiki";
-export const description = "Search something on Wikipedia and get a short summary of it.";
-export const aliases = ["wikipedia"];
+export const name = "wikipedia";
+export const description =
+  "Search something on Wikipedia and get a short summary of it.";
+export const aliases = ["wiki"];
 export const usage = `${Prefix}wiki Lord Of The Rings [Gives A Short Summary from Wikipedia]"`;
 export const category = "Core";
 
 export async function execute(client, message, args) {
-    const command = args.join(" ");
+  const command = args.join(" ");
 
-    let requestLang = "en"; // This is the Wikipedia language in which we send our request.
-    if (!args[0]) {
-        message.react("👎").catch((e) => Logger.error(e));
-        message.channel.send({
-            embed: {
-                color: Colors.RED,
-                description: "The command you gave was invalid or doesnt exist",
-            },
-        });
-    } else {
-        // Using some regex to make the string understandable to the getWikipediaShortSummary() function.
-        let searchValue = args.toString().replace(/,/g, " ");
-        searchValue = searchValue.replace(">" + command + " ", "");
-        // Call the getWikipediaShortSummary() function.
-        getWikipediaShortSummary(message, searchValue, requestLang)
-            .catch((e) => Logger.error(e));
-    }
+  let requestLang = "en"; // This is the Wikipedia language in which we send our request.
+  if (!args[0]) {
+    message.react("👎").catch((e) => Logger.error(e));
+    message.channel.send({
+      embed: {
+        color: Colors.RED,
+        description: "The command you gave was invalid or doesnt exist",
+      },
+    });
+  } else {
+    // Using some regex to make the string understandable to the getWikipediaShortSummary() function.
+    let searchValue = args.toString().replace(/,/g, " ");
+    searchValue = searchValue.replace(">" + command + " ", "");
+    // Call the getWikipediaShortSummary() function.
+    getWikipediaShortSummary(message, searchValue, requestLang).catch((e) =>
+      Logger.error(e)
+    );
+  }
 }
