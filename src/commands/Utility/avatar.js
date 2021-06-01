be32a8b868a16ddc361120538a89426fbc590651
ibra <muhammadibrahimhisham@gmail.com>
2021-06-01 13:09:18 +0500

    avatar command now uses getUserMentioned module

--- src/commands/Utility/avatar.js
+++ src/commands/Utility/avatar.js
@@ -1,26 +1,23 @@
 import { MessageEmbed } from "discord.js";
 import { Prefix } from "../../config.js";
 import { Colors } from "../../colors.js";
+import { getUserMentioned } from "../../modules/mentionedUser.js";
 
 export const name = "avatar";
-export const description = "Returns mentioned user's avatar and returns your avatar if there are no arguments";
+export const description =
+  "Returns mentioned user's avatar and returns your avatar if there are no arguments";
 export const aliases = ["av"];
 export const usage = `${Prefix}avatar @User#001 - Returns Their Avatar`;
 export const category = "Utility";
 
 export async function execute(client, message, args) {
-    const avatarEmbed = new MessageEmbed();
+  const avatarEmbed = new MessageEmbed();
 
-    let user = undefined;
-    if (!message.mentions.users.first()) {
-        user = message.author;
-    } else {
-        user = message.mentions.users.first();
-    }
+  let user = getUserMentioned();
 
-    avatarEmbed.setTitle(`${user.username}'s avatar!`);
-    avatarEmbed.setColor(Colors.ORANGE);
-    avatarEmbed.setImage(user.displayAvatarURL());
+  avatarEmbed.setTitle(`${user.username}'s avatar!`);
+  avatarEmbed.setColor(Colors.ORANGE);
+  avatarEmbed.setImage(user.displayAvatarURL());
 
-    message.channel.send(avatarEmbed);
+  message.channel.send(avatarEmbed);
 }
