065ca4c452ab08feacc89f10eb15fb785bae2936
ibra <muhammadibrahimhisham@gmail.com>
2021-06-01 13:11:10 +0500

    say.js now uses enforceAdmin module

--- src/commands/Admin/say.js
+++ src/commands/Admin/say.js
@@ -1,6 +1,7 @@
 import { MessageEmbed } from "discord.js";
 import { Author, Prefix } from "../../config.js";
 import { Colors } from "../../colors.js";
+import { enforceAdmin } from "../../modules/enforce.js";
 
 export const name = "say";
 export const description = "A command that sends a user-sent message as a bot";
@@ -9,35 +10,31 @@ export const permission = "Bot Owner";
 export const category = "Admin";
 
 export async function execute(client, message, args) {
-    //Only allow the command to work if the author of the command is the bot author.
-    if (message.author.id == Author) {
-        const commandlessMessage = args.join(" ");
-        if (message.mentions.channels.first()) {
-            client.channels.cache
-                .get(message.mentions.channels.first().id)
-                .send(commandlessMessage)
-                .catch((error) => {
-                    let errorEmbed = new MessageEmbed();
-                    if (error.code == 50001) {
-                        errorEmbed
-                            .setColor(Colors.RED)
-                            .setAuthor("> Error 403")
-                            .setDescription("I was unable to access this channel.");
-                    }
+  if (enforceAdmin(message)) {
+    const commandlessMessage = args.join(" ");
+    if (message.mentions.channels.first()) {
+      client.channels.cache
+        .get(message.mentions.channels.first().id)
+        .send(commandlessMessage)
+        .catch((error) => {
+          let errorEmbed = new MessageEmbed();
+          if (error.code == 50001) {
+            errorEmbed
+              .setColor(Colors.RED)
+              .setAuthor("> Error 403")
+              .setDescription("I was unable to access this channel.");
+          }
 
-                    message.channel.send(errorEmbed);
-                });
-        }
-        else {
-            message.channel.send(commandlessMessage);
-        }
+          message.channel.send(errorEmbed);
+        });
     } else {
-        let errorEmbed = new MessageEmbed()
-            .setColor(Colors.RED)
-            .setAuthor("> Error 403")
-            .setDescription(
-                `Only the BotAuthor (${Author})  can use this command!`
-            );
-        message.channel.send(errorEmbed);
+      message.channel.send(commandlessMessage);
     }
+  } else {
+    let errorEmbed = new MessageEmbed()
+      .setColor(Colors.RED)
+      .setAuthor("> Error 403")
+      .setDescription(`Only the BotAuthor (${Author})  can use this command!`);
+    message.channel.send(errorEmbed);
+  }
 }
